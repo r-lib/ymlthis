2d7553d5c7d9b1a3ee50792dbfc7ee28d1f11cc6
Malcolm Barrett <malcolmbarrett@gmail.com>
2019-07-29 12:23:47 -0700

    mention RSC in params docs

--- R/yml_params.R
+++ R/yml_params.R
@@ -3,9 +3,9 @@
 #' R Markdown lets you add dynamic parameters to your report using the `params`
 #' YAML field (see the [R Markdown
 #' book](https://bookdown.org/yihui/rmarkdown/parameterized-reports.html) for
-#' examples). The values of these variables can be called inside your R Markdown
-#' document using `params$field_name`. [There are several ways to change the
-#' values of the
+#' examples); parameterized reports are also used in RStudio Connect. The values
+#' of these variables can be called inside your R Markdown document using
+#' `params$field_name`. [There are several ways to change the values of the
 #' parameters](https://bookdown.org/yihui/rmarkdown/params-knit.html): manually
 #' change the YAML, use the `params` argument in `rmarkdown::render()`, or knit
 #' with parameters, which launches a Shiny app to select values for each.
