f287be29e0c195311876fa670281eee0dd991819
Malcolm Barrett <malcolmbarrett@gmail.com>
2019-08-06 16:25:56 -0700

    switch pandoc_citeproc_exec() to match rmarkdown:::pandoc_citeproc()

--- R/use_pandoc.R
+++ R/use_pandoc.R
@@ -129,6 +129,9 @@ stop_if_pandoc_not_installed <- function() {
 }
 
 pandoc_citeproc_exec <- function() {
-  paste0(rmarkdown::pandoc_exec(), "-citeproc")
+  citeproc_path <- paste0(rmarkdown::pandoc_exec(), "-citeproc")
+  if (file.exists(citeproc_path)) return(citeproc_path)
+
+  "pandoc-citeproc"
 }
 
