b62ec46de1e9f235713d8077060a1e0633f31937
Malcolm Barrett <malcolmbarrett@gmail.com>
2019-08-12 09:41:08 -0700

    add install check for shiny components

--- R/addins.R
+++ R/addins.R
@@ -1,5 +1,25 @@
 #' @importFrom utils getFromNamespace
 launch_yaml_addin <- function() {
+  req_pkgs <- c("shiny", "miniUI", "shinyBS")
+  not_installed <- purrr::map_lgl(req_pkgs, Negate(requireNamespace), quietly = TRUE)
+  if (any(not_installed)) {
+    req_pkgs <- req_pkgs[not_installed]
+    cat_pkgs <- glue::glue_collapse(req_pkgs, sep = ", ")
+    error_msg <- glue::glue("Required packages not found: {cat_pkgs}")
+    info_msg <- glue::glue(
+      "Using the add-in requires the following packages that \\
+      are not installed: {cat_pkgs}."
+    )
+    pkg_string <- glue::glue("'{req_pkgs}'") %>%
+      glue::glue_collapse(", ")
+    if (sum(not_installed) > 1) pkg_string <- glue::glue("c({pkg_string})")
+    install_string <- glue::glue("install.packages({pkg_string})")
+    on.exit({
+      usethis::ui_info(info_msg)
+      usethis::ui_todo("install them with {usethis::ui_code(install_string)}")
+    })
+    stop(error_msg, call. = FALSE)
+  }
   addin_dir <- system.file("addin", "new_yaml", package = "ymlthis")
   app <- shiny::shinyAppDir(addin_dir)
   shiny::runGadget(
